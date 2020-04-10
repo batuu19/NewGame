90c8c9fe25ec87661e165c14500b75b6112003e6
Bartek <bartek.blazkow@o2.pl>
2020-04-10 12:34:59 +0200

    begin dll

---
+++ Engine/MathFunc.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "MathFunc.h"
+
+int Add(const int a, const int b)
+{
+	return a+b;
+}
+
+int Sub(const int a, const int b)
+{
+	return a-b;
+}
+
+int Mul(const int a, const int b)
+{
+	return a*b;
+}
+
+int Div(const int a, const int b)
+{
+	return a/b;
+}
